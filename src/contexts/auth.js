1975f63cb73fb7f23d08e072becf9070bb532e3b
wanersbh <wanersbh@gmail.com>
2023-03-28 10:55:23 -0300

    Ajuste funcionalidade profile

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -13,30 +13,30 @@ function AuthProvider({ children }) {
     const [loading, setLoading] = useState(true);
     const [loadingAuth, setLoadingAuth] = useState(false);
 
-    useEffect(()=>{
-        
-        async function loadStorage(){
+    useEffect(() => {
+
+        async function loadStorage() {
             const storageUser = await AsyncStorage.getItem('@devapp');
 
-            if(storageUser){
+            if (storageUser) {
                 setUser(JSON.parse(storageUser));
                 setLoading(false);
             }
-            
+
             setLoading(false);
         }
 
         loadStorage();
 
-    },[])
+    }, [])
 
     async function signUp(name, email, password) {
-        
+
         setLoadingAuth(true);
-        
+
         await auth().createUserWithEmailAndPassword(email, password).then(async (value) => {
             let uid = value.user.uid;
-            
+
             await firestore().collection('users').doc(uid).set({
                 nome: name,
                 createdAt: new Date()
@@ -46,7 +46,7 @@ function AuthProvider({ children }) {
                     nome: name,
                     email: value.user.email
                 }
-                
+
                 setUser(data);
                 storageUser(data);
                 setLoadingAuth(false);
@@ -59,48 +59,58 @@ function AuthProvider({ children }) {
             setLoadingAuth(false);
         })
     }
-    
+
     async function signIn(email, password) {
         setLoadingAuth(true);
-        
+
         await auth().signInWithEmailAndPassword(email, password).then(async (value) => {
             let uid = value.user.uid;
-            
+
             const userProfile = await firestore().collection('users')
-            .doc(uid).get();
-            
+                .doc(uid).get();
+
             // console.log(userProfile.data().nome);
             let data = {
                 uid: uid,
                 nome: userProfile.data().nome,
                 email: value.user.email
             };
-            
+
             setUser(data);
             storageUser(data);
             setLoadingAuth(false);
-            
+
         }).catch((error) => {
             console.log(error);
             setLoadingAuth(false);
         })
     }
 
-    async function signOut(){
-        await auth().signOut().then( async ()=>{
-            await AsyncStorage.clear().then(()=>{
+    async function signOut() {
+        await auth().signOut().then(async () => {
+            await AsyncStorage.clear().then(() => {
                 setUser(null);
-            
+
             })
         })
     }
 
-    async function storageUser(data){
+    async function storageUser(data) {
         await AsyncStorage.setItem('@devapp', JSON.stringify(data));
     }
 
     return (
-        <AuthContext.Provider value={{ signed: !!user, signIn, signUp, signOut, loadingAuth, loading, user }}>
+        <AuthContext.Provider value={{
+            signed: !!user,
+            signIn,
+            signUp,
+            signOut,
+            loadingAuth,
+            loading,
+            user,
+            setUser,
+            storageUser
+        }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,123 @@
-import React, { useContext } from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useContext, useState } from 'react';
 
 import { AuthContext } from '../../contexts/auth';
 
+import {
+    ButtonText,
+    ButtonUpdate,
+    Container,
+    Email,
+    Name,
+    UploadButton,
+    UploadText,
+    Avatar,
+    ModalContainer,
+    ButtonBack,
+    Input
+} from './styles';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import firestore from '@react-native-firebase/firestore';
+
+import Header from '../../components/Header';
+import { Modal, Platform } from 'react-native';
+
 export default function Profile() {
 
-    const { signOut, user } = useContext(AuthContext);
+    const { signOut, user, setUser, storageUser } = useContext(AuthContext);
+
+    const [nome, setNome] = useState(user?.nome);
+    const [url, setUrl] = useState(null);
+    const [open, setOpen] = useState(false);
+
+    async function handleSignOut() {
+        await signOut();
+    }
+
+    async function updateProfile() {
+        if (nome === '') {
+            return;
+        }
+
+        //Atualiza o nome na collection users
+        await firestore().collection('users').doc(user.uid).update({
+            nome: nome
+        });
+
+        //Buscar todos posts desse user e atulizar o nome dele
+        const postDocs = await firestore().collection('posts')
+            .where('userId', '==', user?.uid).get();
+
+        //Percorrer todos posts desse user e atualizar
+        postDocs.forEach( async doc => {
+            await firestore().collection('posts').doc(doc.id).update({
+                autor: nome
+            })
+        })
+
+        let data = {
+            uid: user.uid,
+            nome: nome,
+            email: user.email 
+        }
+
+        setUser(data);
+        storageUser(data);
+        setOpen(false);
+    }
 
     return (
-        <View>
-            <Text>TELA PROFILE: {user.nome}</Text>
-            <Button title='Sair' onPress={signOut} />
-        </View>
+        <Container>
+            <Header />
+
+            {url ? (
+                <UploadButton onPress={() => alert('CLICOU 1')}>
+                    <UploadText>+</UploadText>
+                    <Avatar
+                        source={{ uri: url }}
+                    />
+                </UploadButton>
+            ) : (
+                <UploadButton onPress={() => alert('CLICOU 2')}>
+                    <UploadText>+</UploadText>
+                </UploadButton>
+            )}
+
+            <Name>{nome}</Name>
+            <Email>{user?.email}</Email>
+
+            <ButtonUpdate bg="#428cfd" onPress={() => setOpen(true)}>
+                <ButtonText color="#FFF">Atualizar Perfil</ButtonText>
+            </ButtonUpdate>
+
+            <ButtonUpdate bg="#DDD" onPress={handleSignOut}>
+                <ButtonText color="#353840">Sair</ButtonText>
+            </ButtonUpdate>
+
+            <Modal visible={open} animationType='slide' transparent={true}>
+                <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+                    <ButtonBack onPress={() => setOpen(false)}>
+                        <Icon
+                            name='arrow-left'
+                            size={22}
+                            color='#121212'
+                        />
+                        <ButtonText color='#121212'>Voltar</ButtonText>
+                    </ButtonBack>
+                    <Input
+                        placeholder={user?.nome}
+                        value={nome}
+                        onChangeText={(text) => setNome(text)}
+                    />
+
+                    <ButtonUpdate bg="#428cfd" onPress={updateProfile}>
+                        <ButtonText color="#FFF">Salvar</ButtonText>
+                    </ButtonUpdate>
+                </ModalContainer>
+
+            </Modal>
+
+        </Container>
     );
 }
